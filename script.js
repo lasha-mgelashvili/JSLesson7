87096fb2b0daa02cfee08d7a486c14217f4e3e3b
Mgela19 <lashamgelashvili777@gmail.com>
2024-01-24 10:55:08 +0400

    remmover

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+// add  &  delete
+
+
+const inputElement = document.getElementById("input");
+const addBtn = document.getElementById("add-btn");
+const ulElement = document.getElementById("ul-element");
+
+addBtn.addEventListener("click", function () {
+  let Inputvalue = inputElement.value;
+  if (Inputvalue === "  ") {
+    return;
+  }
+
+
+  let liElement = document.createElement("li");
+  liElement.textContent = Inputvalue;
+  liElement.style = "color: blue ";
+
+  let deleteBtn = document.createElement("i");
+  deleteBtn.classList.add("fa-solid");
+  deleteBtn.classList.add("fa-trash");
+  deleteBtn.style = "color: #B197FC";
+  deleteBtn.addEventListener("click", () => {
+    liElement.remove();
+  });
+
+  liElement.appendChild(deleteBtn);
+  ulElement.appendChild(liElement);
+
+  // 3
+
+  inputElement.value = " ";
+});
